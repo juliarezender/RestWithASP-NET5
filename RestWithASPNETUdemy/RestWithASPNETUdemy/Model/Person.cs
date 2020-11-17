a18f81e403044f43bff66f4c35f9c43a4639c8fb
Julia Rezende <juliarezender@gmail.com>
2020-11-17 12:44:34 -0300

    working with four verbs

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Moldel
+{
+
+    public class Person
+    {
+        public long Id { get; set; }
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
