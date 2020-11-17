117d1fafbde4b92a4af1ade82c92d8fdaac3c19c
Julia Rezende <juliarezender@gmail.com>
2020-11-17 13:59:45 -0300

    create IPService and  PersonServiceImplementations classes

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
@@ -9,7 +9,7 @@ namespace RestWithASPNETUdemy.Moldel
     public class Person
     {
         public long Id { get; set; }
-        public string FistName { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
         public string Gender { get; set; }
---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/IPersonService.cs
@@ -0,0 +1,16 @@
+using RestWithASPNETUdemy.Moldel;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Services
+{
+
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+    }
+}
