a30ccf13311906d58305d8643d42c29a96289a95
julia <juliarezender@gmail.com>
2021-02-21 12:39:10 -0300

    implement VO

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Data/Converter/Contract/IParser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Data.Converter.Contract
+{
+    public interface IParser<Origin, Destination>
+    {
+        Destination Parse(Origin origin);
+        List<Destination> Parse(List<Origin> origin);
+    }
+}
