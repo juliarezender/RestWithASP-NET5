5a24a898d1dc5a6dcc0a12bfb7b277224a9cf987
julia <juliarezender@gmail.com>
2021-03-02 15:41:31 -0300

    Implement HATEOAS

--- RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V4__Populate_Table_Book.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
-	(1, 'São Paulo - Brasil', 'Airton', 'Male', 'Senna'),
-	(2, 'Anchiano - Itália', 'Leonardo', 'Male', 'da Vinci'),
-	(4, 'Ivezo - South Africa', 'Nelson', 'Male', 'Mandela');
