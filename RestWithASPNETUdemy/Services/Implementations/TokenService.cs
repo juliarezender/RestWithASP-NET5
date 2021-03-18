7202cad41af9fc6a4009b0e5c02bb40be4f433e7
julia <julia@DESKTOP-15FBI5U>
2021-03-18 13:55:15 -0300

    Creating AuthController

--- RestWithASPNETUdemy/Services/Implementations/TokenService.cs
+++ RestWithASPNETUdemy/Services/Implementations/TokenService.cs
@@ -21,13 +21,13 @@ namespace RestWithASPNETUdemy.Services.Implementations
         public string GenerateAccessToken(IEnumerable<Claim> claims)
         {
             var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.Secret));
-            var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
             var tokenOptions = new JwtSecurityToken(
                 issuer: _configuration.Issuer,
                 audience: _configuration.Audience,
                 claims: claims,
                 expires: DateTime.Now.AddMinutes(_configuration.Minutes),
-                signingCredentials: signingCredentials
+                signingCredentials: signinCredentials
             );
             string tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
 
