0347d69800a00037c373c1f812a1b4dd0621af03
julia <julia@DESKTOP-15FBI5U>
2021-04-05 10:56:10 -0300

    Implement paged search at generic repository

--- RestWithASPNETUdemy/Repository/Generic/IRepository.cs
+++ RestWithASPNETUdemy/Repository/Generic/IRepository.cs
@@ -1,5 +1,4 @@
-﻿using RestWithASPNETUdemy.Model;
-using RestWithASPNETUdemy.Model.Base;
+﻿using RestWithASPNETUdemy.Model.Base;
 using System.Collections.Generic;
 
 namespace RestWithASPNETUdemy.Repository
@@ -7,10 +6,19 @@ namespace RestWithASPNETUdemy.Repository
     public interface IRepository<T> where T : BaseEntity
     {
         T Create(T item);
+
         T FindByID(long id);
+
         List<T> FindAll();
+
         T Update(T item);
+
         void Delete(long id);
+
         bool Exists(long id);
+
+        List<T> FindWithPagedSearch(string query);
+
+        int GetCoutn(string query);
     }
 }
